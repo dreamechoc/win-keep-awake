0835fd06fe25450aa520398f9f020239d5d611b6
echo <131730381+dreamechoc@users.noreply.github.com>
2025-12-01 11:46:52 +0800

    Unblock all .bat and .ps1 files in start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
 @echo off
-powershell -NoProfile -Command "Get-Item '%~dp0Awake-Start.ps1' | Unblock-File"
+powershell -NoProfile -Command "Get-Item '%~dp0*.bat' | Unblock-File"
+powershell -NoProfile -Command "Get-Item '%~dp0*.ps1' | Unblock-File"
 powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0Awake-Start.ps1"
+
